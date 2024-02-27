48d81675e24812faa3af82e3f9751b817814a244
8n9ght <jean.arthur.thoni@gmail.com>
2024-02-27 18:30:45 +0100

    forget in between commit, final result

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import data from "./data.json"
+
 
 function App() {
+  const [isActive, setIsActive] = useState(0)
+
+  const handleAccordion = () => {
+
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+    <div className="faq">
+      <header className="faq__header">
+        <img className="faq__header__img" src="./images/icon-star.svg" alt="faq icon"></img>
+        <h1>FAQs</h1>
       </header>
+      {data.map((item, i) => {
+        return (
+          <div className="faq__card" key={i}>
+        <article className="faq__card__header">
+          <h2 className="faq__card__header__title">
+          {item.title}
+          </h2>
+          <img className="faq__card__header__img" src={isActive === i ? "./images/icon-minus.svg" : "./images/icon-plus.svg"} alt="minus icon" onClick={
+            () => {
+                if(isActive === i){
+                  setIsActive(null)
+                } else {
+                  setIsActive(i)
+                }
+             }
+          }></img>
+        </article>
+        {isActive === i && <article className="faq__card__content">
+           <p> {data[i].content}</p>
+        </article>}
+        {i !== (data.length - 1) && <span className="faq__card__line"></span>}
+      </div>
+        )
+      })}
+      
     </div>
+  </div>
   );
 }
 
